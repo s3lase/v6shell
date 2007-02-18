f697d5f7704b6a593428d7f7a9a276a3bcfc8cdd
J.A. Neitzel <jan@v6shell.org>
2007-02-18 16:03:34 +0000

    Add ident $Id$ keyword.

--- osh.c
+++ osh.c
@@ -26,7 +26,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *	@(#)$Id:$
+ *	@(#)$Name$: $Id$
  */
 /*
  *	Derived from:
@@ -76,7 +76,7 @@
 
 #ifndef	lint
 #include "version.h"
-OSH_RCSID("$Id:$");
+OSH_RCSID("$Name$: $Id$");
 #endif	/* !lint */
 
 #include <sys/types.h>
