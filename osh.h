929bbfeea3b345d2e6ff5128500c0af715093b70
J.A. Neitzel <jan@v6shell.org>
2008-10-18 22:22:37 +0000

    Move common definitions to defs.h instead.

--- osh.h
+++ osh.h
@@ -33,84 +33,19 @@
 #define	OSH_H
 
 /*
- * Following standard conventions, file descriptors 0, 1, and 2 are used
- * for standard input, standard output, and standard error respectively.
- */
-#define	FD0		STDIN_FILENO
-#define	FD1		STDOUT_FILENO
-#define	FD2		STDERR_FILENO
-
-/*
- * Exit status values
- */
-#define	FC_ERR		124	/* fatal child error (changed in pwait()) */
-#define	SH_ERR		2	/* shell-detected error (default value)   */
-#define	SH_FALSE	1
-#define	SH_TRUE		0
-
-/*
- * Diagnostics
- */
-#define	ERR_PAREN	") expected"
-#define	ERR_ALTOOLONG	"Arg list too long"
-#define	ERR_FORK	"Cannot fork - try again"
-#define	ERR_PIPE	"Cannot pipe - try again"
-#define	ERR_TRIM	"Cannot trim"
-#define	ERR_ALINVAL	"Invalid argument list"
-#define	ERR_AVIINVAL	"Invalid argv index"
-#define	ERR_NODIR	"No directory"
-#define	ERR_NOHOMEDIR	"No home directory"
-#define	ERR_NOMATCH	"No match"
-#define	ERR_NOPWD	"No previous directory"
-#define	ERR_NOSHELL	"No shell!"
-#define	ERR_NOTTY	"No terminal!"
-#define	ERR_NOMEM	"Out of memory"
-#define	ERR_SETID	"Set-ID execution denied"
-#define	ERR_TMARGS	"Too many args"
-#define	ERR_TMCHARS	"Too many characters"
-#define	ERR_ARGCOUNT	"arg count"
-#define	ERR_ARGUMENT	"argument expected"
-#define	ERR_BADDIR	"bad directory"
-#define	ERR_BADMASK	"bad mask"
-#define	ERR_BADNAME	"bad name"
-#define	ERR_BADSIGNAL	"bad signal"
-#define	ERR_CREATE	"cannot create"
-#define	ERR_EXEC	"cannot execute"
-#define	ERR_OPEN	"cannot open"
-#define	ERR_SEEK	"cannot seek"
-#define	ERR_COMMAND	"command expected"
-#define	ERR_DIGIT	"digit expected"
-#define	ERR_GENERIC	"error"
-#define	ERR_EXPR	"expression expected"
-#define	ERR_LABNOTFOUND	"label not found"
-#define	ERR_LABTOOLONG	"label too long"
-#define	ERR_NOARGS	"no args"
-#define	ERR_NOTDIGIT	"not a digit"
-#define	ERR_NOTFOUND	"not found"
-#define	ERR_OPERATOR	"operator expected"
-#define	ERR_SYNTAX	"syntax error"
-#define	ERR_OPUNKNOWN	"unknown operator"
-#define	ERR_BRACE	"} expected"
-#define	FMT1S		"%s\n"
-#define	FMT2S		"%s: %s\n"
-#define	FMT3S		"%s: %s: %s\n"
-
-#define	EQUAL(a, b)	(*(a) == *(b) && strcmp((a), (b)) == 0)
-
-/*
  * Shell special built-in (sbi) command keys
  */
 enum sbikey {
-	SBI_NULL,   SBI_CHDIR,  SBI_EXEC,     SBI_EXIT,
-	SBI_FD2,    SBI_GOTO,   SBI_IF,       SBI_LOGIN,
-	SBI_NEWGRP, SBI_SETENV, SBI_SHIFT,    SBI_SIGIGN,
-	SBI_SOURCE, SBI_UMASK,  SBI_UNSETENV, SBI_WAIT,
-	SBI_UNKNOWN
+	SBI_NULL,   SBI_CHDIR,  SBI_ECHO,     SBI_EXEC,
+	SBI_EXIT,   SBI_FD2,    SBI_GOTO,     SBI_IF,
+	SBI_LOGIN,  SBI_NEWGRP, SBI_SETENV,   SBI_SHIFT,
+	SBI_SIGIGN, SBI_SOURCE, SBI_UMASK,    SBI_UNSETENV,
+	SBI_WAIT,   SBI_UNKNOWN
 };
 
 extern	uid_t	euid;	/* effective shell user ID */
 
-enum sbikey	cmd_key_lookup(const char *);
+enum sbikey	cmd_lookup(const char *);
 void		fd_print(int, const char *, /*@printflike@*/ ...);
 void		omsg(int, const char *, va_list);
 int		uexec(enum sbikey, int, char **);
